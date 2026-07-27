10a4f3330351e0f813aa4c4ad8798554dfb0ad02
HarshadMandola <mandolaharshad6@gmail.com>
2026-07-27 23:39:40 +0530

    added frontend and some measures

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-black h-screen flex items-center justify-center'>
+      <button className='bg-white w-50 h-15 ' onClick={}>
+        login with google
+      </button>
+    </div>
+  )
+}
+
+export default App
